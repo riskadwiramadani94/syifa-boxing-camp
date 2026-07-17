a13d7c7fdf5fe9cdd45ffada4af8e3fd2dc0510d
riskadwiramadani94 <riskadwiramadani94.com>
2026-07-18 00:14:58 +0700

    fix: perbaiki konfigurasi Cloudinary disk driver

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,11 +61,9 @@ return [
         ],
 
         'cloudinary' => [
-            'driver' => 'cloudinary',
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-            'api_key' => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
-            'secure' => true,
+            'driver'  => 'cloudinary',
+            'url'     => env('CLOUDINARY_URL'),
+            'secure'  => true,
         ],
 
     ],
